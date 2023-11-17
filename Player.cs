f0332effbd63e405dd84ea5980209c67c4d72cf7
fymmit <blomqvist.timo@gmail.com>
2023-11-17 13:30:25 +0200

    Add basic Player and Wall scenes

---
+++ Player.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Vector2.Zero;
+
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
+		{
+			velocity = direction * Speed;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
